e83588fd6492b63e8bc73ddb3074c82d5f878319
hnlim <email@>
2023-09-13 20:58:42 -0500

    final variable

---
+++ src/main/java/topic2/FinalVariable.java
@@ -0,0 +1,14 @@
+package topic2;
+
+public class FinalVariable {
+    public static void main(String[] args) {
+
+        // when you put final in front of the variable type, the value of the variable won't change
+        // also, it is common to write the final variable name in uppercase letters
+        final int NUMBER_OF_DAYS = 7;
+
+        // when trying to rewrite the final variable, error occur
+        NUMBER_OF_DAYS = 8;
+
+    }
+}
