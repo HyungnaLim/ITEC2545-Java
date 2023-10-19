28a725ceff75b0c054c45cfd2cf369670db6c7d1
hnlim <email@>
2023-10-19 02:20:45 -0500

    exception handling - try,catch

---
+++ src/main/java/topic6/HelloReading2.java
@@ -0,0 +1,41 @@
+package topic6;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class HelloReading2 {
+    public static void main(String[] args) {
+
+        try {
+            // FileReader can open, close, and read the file character by character
+            FileReader reader = new FileReader("hello.txt");
+
+            // BufferedReader provides readLine method
+            // it collects all the character from one line and put them together into a String, and return the string
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            // you can create both FileReader and BufferedReader in this way
+            BufferedReader bufferedReader2 = new BufferedReader(new FileReader("hello.txt"));
+
+            // to read an entire file, use a while loop that repeats while readLine() != null
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+
+            // shorter code to read the entire file
+            String line2;
+            while ((line2 = bufferedReader2.readLine()) != null) {
+                System.out.println(line2);
+            }
+            bufferedReader2.close();
+
+        } catch (IOException e) { // only runs if IOException is thrown
+            System.out.println("Sorry, file not found.");
+        }
+
+    }
+}
